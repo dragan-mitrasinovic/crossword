f744c8f7b6a0b0e4ba9a83f7c4412e7ae609eadf
dragan <dragan.mitrasinovic19@gmail.com>
2022-12-24 23:31:16 +0100

    Initial ArcConsistency, I think I'm right

--- algorithms.py
+++ algorithms.py
@@ -1,4 +1,5 @@
 import copy
+import itertools
 
 
 class Algorithm:
@@ -98,7 +99,29 @@ def forward_check_search(tiles: list, variables: dict, domains: dict, solutions:
 
 
 def arc_consistency_search(tiles: list, variables: dict, domains: dict, solutions: list, indexes_to_pick: list):
-    return
+    level = 0
+    while level != len(variables):
+        current_variable = list(variables.keys())[level]
+
+        if any_domain_is_empty(domains):
+            domains = solutions[-2][2]
+            last_variable = list(variables.keys())[level - 1]
+            delete_word(tiles, last_variable, variables[last_variable])
+            level -= 1
+            continue
+
+        current_value_index = indexes_to_pick[level]
+        indexes_to_pick[level] += 1
+        if is_consistent_assignment(tiles, current_variable, domains[current_variable][current_value_index]):
+            old_domains = domains
+            if arc_consistency_check_fails(tiles, variables, domains, level):
+                domains = old_domains
+                continue
+            forward_check(tiles, variables, domains, level, domains[current_variable][current_value_index])
+            solutions.append([current_variable, current_value_index, copy.deepcopy(domains)])
+            add_word(tiles, current_variable, domains[current_variable][current_value_index])
+            level += 1
+        continue
 
 
 def is_consistent_assignment(tiles: list, variable: str, word: str):
@@ -134,15 +157,55 @@ def forward_check(tiles: list, variables: dict, domains: dict, level: int, word:
     return True
 
 
+def arc_consistency_check_fails(tiles: list, variables: dict, domains: dict, level: int):
+    new_word_letter_positions = get_index_list(tiles, variables, list(variables.keys())[level])
+    possibly_impacted_variables = []
+
+    for variable in list(variables.keys())[level + 1:]:
+        variable_letter_positions = get_index_list(tiles, variables, variable)
+        possible_conflict_positions = list(set(new_word_letter_positions).intersection(variable_letter_positions))
+        if len(possible_conflict_positions) != 0:
+            possibly_impacted_variables.append(variable)
+
+    for pair in itertools.permutations(possibly_impacted_variables, 2):
+        x = pair[0]
+        y = pair[1]
+        x_letter_positions = get_index_list(tiles, variables, x)
+        y_letter_positions = get_index_list(tiles, variables, y)
+        possible_conflict_positions = list(set(x_letter_positions).intersection(y_letter_positions))
+        if len(possible_conflict_positions) == 0:
+            continue
+        for word_x in list(domains[x]):
+            breaks_consistency = True
+            for word_y in domains[y]:
+                if are_compatible(x_letter_positions, y_letter_positions, word_x, word_y):
+                    breaks_consistency = False
+                    break
+            if breaks_consistency:
+                domains[x].remove(word_x)
+    return any_domain_is_empty(domains)
+
+
+def are_compatible(x_letter_positions: range, y_letter_positions: range, word_x: str, word_y: str):
+    possible_conflict_positions = list(set(x_letter_positions).intersection(y_letter_positions))
+
+    for index in possible_conflict_positions:
+        if word_x[x_letter_positions.index(index)] != word_y[y_letter_positions.index(index)]:
+            return False
+    return True
+
+
 def delete_word(tiles: list, variable: str, length: int):
     index = int(variable[:-1])
     is_horizontal = variable[-1] == "h"
+
     for i in range(length):
         current_mark = get_matrix_element(tiles, index)
         if len(current_mark) == 1:
             set_matrix_element(tiles, index, False)
         else:
             set_matrix_element(tiles, index, current_mark[:-1])
+
         index = inc_matrix_index(tiles, index, is_horizontal)
     return
 
@@ -150,12 +213,14 @@ def delete_word(tiles: list, variable: str, length: int):
 def add_word(tiles: list, variable: str, word: str):
     index = int(variable[:-1])
     is_horizontal = variable[-1] == "h"
+
     for char in word:
         current_mark = get_matrix_element(tiles, index)
         if not current_mark:
             set_matrix_element(tiles, index, char)
         else:
             append_to_matrix_element(tiles, index, char)
+
         index = inc_matrix_index(tiles, index, is_horizontal)
     return
 
@@ -164,6 +229,7 @@ def get_index_list(tiles: list, variables: dict, variable: str):
     width = len(tiles[0])
     start_index = int(variable[:-1])
     is_horizontal = variable[-1] == "h"
+
     if is_horizontal:
         return range(start_index, start_index + variables[variable])
     else:
@@ -187,6 +253,7 @@ def append_to_matrix_element(tiles: list, index: int, postfix):
 
 def inc_matrix_index(tiles: list, index: int, is_horizontal_mode: bool):
     width = len(tiles[0])
+
     if is_horizontal_mode:
         index += 1
     else:
